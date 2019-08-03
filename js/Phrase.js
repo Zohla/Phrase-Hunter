69fac46748fa5e59b91d088feaf7ecc7a45a238f
Maren Lilleberre <marenlilleberre@gmail.com>
2019-08-03 11:13:47 +0200

    addPhraseToDisplay method

--- js/Phrase.js
+++ js/Phrase.js
@@ -4,10 +4,34 @@ class Phrase {
         this.phrase = phrase.toLowerCase();
 
     }
-
+    /**
+    * Display phrase on game board.
+    * Takes the phrase, splits it, and adds placeholders and spaces.
+    */
     addPraseToDisplay(){
+        const phraseUl = document.getElementById('phrase');
+        const splitPhrase = this.phrase.split('');
 
-    }
+        splitPhrase.forEach(element => {
+            let phraseLi = document.createElement('li');
+            phraseUl.append(phraseLi);
+
+            if (element === ' ') {
+                li.setAttribute('class', 'space' )
+
+            } else {
+                li.classList.add('letter', `${element}`);
+                li.innerText = element;
+            }
+            
+        });
+        
+
+
+
+
+
+    };
 
     checkLetter() {
 
