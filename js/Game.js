b673c4aacc4c954c446618c595eb17d52dafeabc
Maren Lilleberre <marenlilleberre@gmail.com>
2019-08-04 17:17:51 +0200

    update Game.js

--- js/Game.js
+++ js/Game.js
@@ -5,7 +5,7 @@ class Game {
         this.missed = 0;
         this.phrases = this.createPhrases();
         this.activePhrase = null;
-    }
+    };
 
     /**
    * Creates phrases for use in game
@@ -22,7 +22,7 @@ class Game {
         ];
         return gamePhrases;
 
-    }
+    };
 
     /**
    * Selects random phrase from phrases property
@@ -31,20 +31,66 @@ class Game {
     getRandomPhrase() {
         const randomPhrase = Math.floor(Math.random() * this.phrases.length);
         return this.phrases[randomPhrase];
-    }
+    };
     /**
     * Begins game by selecting a random phrase and displaying it to user
     */
     startGame(){
          const startScreenOverlay = document.getElementById('overlay');
-
          startScreenOverlay.style.display = 'none';
-
-         this.activePhrase = this.getRandomPhrase();
-
-         
+         this.activePhrase = this.getRandomPhrase();         
          this.activePhrase.addPhraseToDisplay();
-        
+    };
 
-    }
+    handleInteraction(){
+
+    };
+
+    /**
+    * Checks for winning move
+    * @return {boolean} True if game has been won, false if game wasn't won
+    */
+    checkForWin() {
+        let phraseDiv = document.getElementById('phrase');
+
+        if (phraseDiv.children.length == phraseDiv.getElementsByClassName('show').lenght) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    /**
+    * Increases the value of the missed property
+    * Removes a life from the scoreboard
+    * Checks if player has remaining lives and ends game if player is out
+    */
+    removeLife() {
+
+        const hearts = document.querySelectorAll('img');
+
+        hearts[this.missed].src="images/lostHeart.png";
+        this.missed += 1;
+        if(this.missed === 5) {
+            this.gameOver(true);
+        } else {
+            this.gameOver(false) //do I need this`?
+        }   
+    };
+
+    /**
+    * Displays game over message
+    * @param {boolean} gameWon - Whether or not the user won the game
+    */
+    gameOver(gameWon) {
+        document.getElementById('overlay').style.display = 'block';//can i use initial here?
+        const gameOverMessage = document.getElementById("game-over-message");
+        if (this.checkForWin === true) {
+            gameOverMessage.innerText = 'You won!';
+        } else {
+            gameOverMessage.innerText = 'Better luck next time!';
+        }
+
+
+    };
 }
