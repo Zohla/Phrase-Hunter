9ea41b8f77736ff12757b843beed2655d58e5b51
Maren Lilleberre <marenlilleberre@gmail.com>
2019-08-04 22:33:09 +0200

    add eventlistener to onscreen keyboard

--- js/app.js
+++ js/app.js
@@ -2,12 +2,20 @@
 
 
 let game;
-const startButton = document.getElementById('btn__reset');
+const startButton = document.getElementById('btn__reset'); //refactor later
 
 startButton.addEventListener('click', function(){
     game = new Game();
     game.startGame();
+    
 
+});
+
+document.getElementById('qwerty').addEventListener('click', (e) => {
+    if(e.target.className === 'key'){
+        game.handleInteraction(e);
+    }
+    
 });
 
 
