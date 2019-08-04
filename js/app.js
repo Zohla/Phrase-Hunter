9418679ecf73501ad9147554cab7bb38e05d8f36
Maren Lilleberre <marenlilleberre@gmail.com>
2019-08-04 13:56:43 +0200

    add eventListener to start button

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * app.js */
+// / * app.js */
 
-const game = new Game();
-game.getRandomPhrase().addPhraseToDisplay();
+
+let game;
+const startButton = document.getElementById('btn__reset');
+
+startButton.addEventListener('click', function(){
+    game = new Game();
+    game.startGame();
+
+});
+
+
+
+   
